015b4a31dfbc618827990759cc6988f69f90bee9
Deepesh-7879 <deepeshajaykasa@gmail.com>
2026-01-27 20:53:12 +0530

    Add files via upload

---
+++ assign-7.js
@@ -0,0 +1,44 @@
+//Hands on-1
+/*
+let date=new Date()
+console.log(date.toString());
+console.log(date.getFullYear());
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+let monthIndex=date.getMonth()
+console.log(monthNames[monthIndex]);
+const days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[date.getDay()]);
+console.log(date.getDate());
+console.log(date);
+*/
+
+//Hands on-2
+/*
+let d1 = new Date("2026-01-20");
+let d2=new Date()
+if(d1 > d2){
+    console.log('enrollment opened');
+}
+else{
+    console.log('enrollment closed');  
+}
+let d3=new Date("2026-02-30")
+console.log(isNandate.getTime());
+if (isNaN(d3.getTime())) {
+  console.log(`Input "${invalidDateInput}" is Invalid`); 
+} else {
+  console.log(`Input "${invalidDateInput}" is Valid`);
+}
+
+*/
+
+//Hands on-3
+/*
+let dob = new Date("2000-05-15");
+let dob2=new Date("2026-01-21");
+
+let years=dob2.getFullYear()-dob.getFullYear()
+console.log(years);
+*/
